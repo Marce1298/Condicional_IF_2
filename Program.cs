30840af7e450051d8a192c9f16dd251bdbe59025
Marce1298 <marchebordon@gmail.com>
2020-09-24 00:35:08 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CondicionalIF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a evaluar si puedes conducir un vehiculo");
+
+            Console.WriteLine("Introduce tu edad, por favor");
+
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad < 18) Console.WriteLine("No puedes conducir vehiculos");
+
+            else 
+            {
+                Console.WriteLine("¿Tienes carnet?");
+
+                string carnet = Console.ReadLine();
+
+                int compara = string.Compare(carnet, "si", true);
+
+                if (compara == 0) Console.WriteLine("Puedes conducir vehiculos");
+
+                else Console.WriteLine("Lo siento mucho no puedes conducir vehiculos");
+            
+            }
+        }
+    }
+}
